9da162b8b17cc59c2bde1eb5335eebca6fa94874
ardinnugraha2001@gmail.com <ardinnugraha2001@gmail.com>
2024-05-05 16:25:50 +0700

    middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,20 @@
-import withAuth from "./middlewares/withAuth";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export function mainMiddleware(request: NextRequest) {
-  return NextResponse.next();
+// function harus diberi nama middleware atau export default (bisa berupa nama apa saja)
+// This function can be marked `async` if using `await` inside
+export function InnerMiddleware(request: NextRequest) {
+  const auth = false;
+  if (auth) {
+    return NextResponse.next();
+  }
+  return NextResponse.redirect(new URL("/auth/login", request.url));
 }
-export default withAuth(mainMiddleware, ["/account","/products/isr","/admins"]);
+
+export default function MainMiddleware(request: NextRequest) {
+  return InnerMiddleware(request);
+}
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: ["/admins", "/account"],
+};
